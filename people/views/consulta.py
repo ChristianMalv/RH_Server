ca37dc6d234925b91087e20d97de04fba4a58e53
server <you@example.com>
2023-11-10 12:58:40 -0600

    update error 400

--- people/views/consulta.py
+++ people/views/consulta.py
@@ -2,11 +2,13 @@
 from people.models import  Person
 from django.http.response import JsonResponse
 def ValidateRFC(request, rfc):
+    status = 200
     if request.method == 'GET':
         try:
             person = Person.objects.get(rfc__unaccent__icontains=rfc)
             person_data={"nombre": person.nombres, "apellido1": person.apellido1, "apellido2": person.apellido2,"matricula":person.matricula, "curp":person.curp } 
         except Person.DoesNotExist:
+          status = 400 
           person_data={"error":"RFC no encontrado"}
-    return JsonResponse(person_data)    
+    return JsonResponse(person_data,status=status)    
       
